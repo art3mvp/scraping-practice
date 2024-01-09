17e93b9d289ae74a0f0f3e7d9cf5e691edae50f1
art3mvp <art3mvp@gmail.com>
2024-01-09 20:03:07 +0000

    scraping simple website

--- main.py
+++ main.py
@@ -1,39 +1,80 @@
 import requests
 from bs4 import BeautifulSoup
-from itertools import cycle
 import lxml
-import json
-
-
-def get_columns(r, tag):
-    columns = f'{tag}:nth-child(1), {tag}:nth-child(2), {tag}:nth-child(5), {tag}:nth-child(8)'
-    return [int_type(i.text) for i in r.select(columns)]
-
-
-def int_type(data):
-    return data if not data.isdigit() else int(data)
-
-
-def valid(data_list):
-    if data_list[-1] <= 4000000 and data_list[1] >= 2005 and data_list[2] == "Бензиновый":
-        return data_list
+import csv
 
 
 def main():
-    response = requests.get(url='https://parsinger.ru/4.8/6/index.html')
-    response.encoding = 'utf-8'
-    soup = BeautifulSoup(response.text, 'lxml')
 
-    header, *rows = soup.find_all('tr')
-    header = get_columns(header, 'th')
-    all_cars = []
 
-    for i in rows:
-        if car:= valid(get_columns(i, 'td')):
-            all_cars.append(dict(zip(header, car)))
-        
-        
-    print(json.dumps(sorted(all_cars, key=lambda x: x['Стоимость авто']), indent=4, ensure_ascii=False))
+    def resoup_func(url):
+        """
+        Make an HTTP request to the given URL, handle potential exceptions, and return a BeautifulSoup object.
+
+        Parameters:
+        - url: to make the request to.
+
+        Returns:
+        - BeautifulSoup or None: Successful parsing or None in case of an error.
+        """
+        try:
+            res = rs.get(url=url)
+            res.encoding = 'utf-8'
+            soup = BeautifulSoup(res.text, 'lxml')
+            return soup
+        except requests.RequestException as e:
+            print(f'Error: {e}\n')
+
+
+    with open('file.csv', 'w', encoding='utf-8', newline='') as file:
+
+        header = 'Наименование;Артикул;Бренд;Модель;Наличие;Цена;Старая цена;Ссылка;'.split(';')
+        writer = csv.writer(file, delimiter=';')
+        writer.writerow(header)
+
+        with requests.Session() as rs:
+            
+            original_url = 'https://parsinger.ru/html/index1_page_1.html'
+            base = original_url.rsplit('/', 1)[0]
+            resoup = resoup_func(original_url)
+
+            if resoup:
+                for index in resoup.find(class_='nav_menu').find_all('a'):
+                    index_resoup = resoup_func(f'{base}/{index["href"]}')
+
+                    if index_resoup is None:
+                        continue
+
+                    for page in index_resoup.find(class_='pagen').find_all('a'):
+                        page_resoup = resoup_func(f'{base}/{page["href"]}')
+
+                        if page_resoup is None:
+                            continue
+
+                        for item in page_resoup.find_all(class_='name_item'):
+
+                            item_url = f'{base}/{item["href"]}'
+                            item_resoup = resoup_func(item_url)
+                            
+                            try:
+                                # Extract data
+                                name = item_resoup.find(id='p_header').text.strip()
+                                # Article, brand, model and in_stock
+                                art_brand_mod_stock = (
+                                    i.text.split(': ')[1].strip() 
+                                    for i in item_resoup.select('.article, #brand, #model, #in_stock')
+                                    )
+                                prices = (p.text.strip() for p in  item_resoup.select('#price, #old_price'))
+
+                                # Write to CSV
+                                writer.writerow((name, *art_brand_mod_stock, *prices, item_url))
+
+                            except Exception as e:
+                                print(f'Error processing item: {item_url}')
+                                print(f'Error: {e}')
+                                continue
+            else:
+                print(f'Something went wrong with the url: {original_url}\n\nFix it and try again')
 
 
 if __name__ == '__main__':
