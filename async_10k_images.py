93d868ca00c31406a2f0889569fedacb0120bf5e
art3mvp <art3mvp@gmail.com>
2024-01-26 22:50:21 +0000

    my biggest parser

---
+++ async_10k_images.py
@@ -0,0 +1,65 @@
+import time
+import aiofiles
+import asyncio
+import aiohttp
+from bs4 import BeautifulSoup
+import os
+from urllib.parse import urljoin
+from itertools import chain
+
+
+def get_folder_size(filepath, size=0):
+    for root, dirs, files in os.walk(filepath):
+        for f in files:
+            size += os.path.getsize(os.path.join(root, f))
+    return size
+
+
+async def write_file(session, url, name):
+    async with aiofiles.open(f'images/{name}', mode='wb') as file:
+        async with session.get(url) as response:
+            async for x in response.content.iter_chunked(5120):
+                await file.write(x)
+    
+
+async def get_pages_urls(session, web_url):
+    async with session.get(web_url) as response:
+        soup = BeautifulSoup(await response.text(), 'lxml')
+        pages = [urljoin(web_url, i['href']) for i in soup.find_all(class_='lnk_img')]
+        return pages
+
+
+async def get_pages_urls_level2(session, web_url):
+    async with session.get(web_url) as response:
+        soup = BeautifulSoup(await response.text(), 'lxml')
+        pages = [urljoin(web_url, i['href']) for i in soup.find_all(class_='lnk_img')]
+        return pages
+
+
+async def get_images_urls(session, url, semaphore):
+    async with semaphore:
+        async with session.get(url) as response:
+            soup = BeautifulSoup(await response.text(), 'lxml')
+            img_urls = [urljoin(url, img['src']) for img in soup.find_all(class_='picture')]
+            return img_urls
+
+
+async def main():
+    web_url = 'https://parsinger.ru/asyncio/aiofile/3/index.html'
+    semaphore = asyncio.Semaphore(20)
+    async with aiohttp.ClientSession() as session:
+        pages_urls = await get_pages_urls(session, web_url)
+        task = [get_pages_urls_level2(session, x) for x in pages_urls]
+        inner_pages = await asyncio.gather(*task) #[[urls...],[urls...]]
+        inner_task = [get_images_urls(session, page, semaphore) for page in chain.from_iterable(inner_pages)]
+        lists_images = await asyncio.gather(*inner_task)
+        images_urls = [write_file(session, url, url.rsplit('/',1)[1]) for url in set(chain.from_iterable(lists_images))]
+        print(len(images_urls))
+        await asyncio.gather(*images_urls)
+
+
+start = time.perf_counter()
+asyncio.run(main())
+print(get_folder_size('images'))
+end = time.perf_counter()
+print(end - start)
--- main.py
+++ main.py
@@ -4,10 +4,7 @@ import lxml
 import csv
 
 
-def main():
-
-
-    def resoup_func(url):
+def resoup_func(url, session):
         """
         Make an HTTP request to the given URL, handle potential exceptions, and return a BeautifulSoup object.
 
@@ -18,12 +15,14 @@ def main():
         - BeautifulSoup or None: Successful parsing or None in case of an error.
         """
         try:
-            res = rs.get(url=url)
+            res = session.get(url=url)
             res.encoding = 'utf-8'
             soup = BeautifulSoup(res.text, 'lxml')
             return soup
         except requests.RequestException as e:
             print(f'Error: {e}\n')
+
+def main():
 
 
     with open('file.csv', 'w', encoding='utf-8', newline='') as file:
@@ -36,17 +35,17 @@ def main():
             
             original_url = 'https://parsinger.ru/html/index1_page_1.html'
             base = original_url.rsplit('/', 1)[0]
-            resoup = resoup_func(original_url)
+            resoup = resoup_func(original_url, rs)
 
             if resoup:
                 for index in resoup.find(class_='nav_menu').find_all('a'):
-                    index_resoup = resoup_func(f'{base}/{index["href"]}')
+                    index_resoup = resoup_func(f'{base}/{index["href"]}', rs)
 
                     if index_resoup is None:
                         continue
 
                     for page in index_resoup.find(class_='pagen').find_all('a'):
-                        page_resoup = resoup_func(f'{base}/{page["href"]}')
+                        page_resoup = resoup_func(f'{base}/{page["href"]}', rs)
 
                         if page_resoup is None:
                             continue
@@ -54,7 +53,7 @@ def main():
                         for item in page_resoup.find_all(class_='name_item'):
 
                             item_url = f'{base}/{item["href"]}'
-                            item_resoup = resoup_func(item_url)
+                            item_resoup = resoup_func(item_url, rs)
                             
                             try:
                                 # Extract data
